c43179a7317449a9a825ed21fe9de68dc3e28801
Mohammad Zakery <95560413+zakery1369@users.noreply.github.com>
2023-09-23 09:22:38 +0330

    Create Discord.sh

---
+++ Discord.sh
@@ -0,0 +1,25 @@
+user_id="xxxxxxxxx"
+bot_token="xxxxxxxxxxxxx"
+channel_id="xxxxxxxxxx"
+webhook_url="https://discord.com/api/webhooks/xxxxxxxxx"
+
+echo "Enter Message :"
+read message
+
+# For Send Message in Channel With Webhook  --> Edit Channel --> Integrations --> Webhooks --> New Webhook --> Copy Webhook URL
+curl -H "Content-Type: application/json" -X POST $webhook_url \
+-d "{\"content\":\"$message\"}"
+
+
+# For Send Message in Channel With Your Bot
+curl -H "Authorization: Bot $bot_token" -H "Content-Type: application/json" -X POST \
+-d "{\"content\":\"$message\"}" "https://discord.com/api/v9/channels/$channel_id/messages"
+
+# For Send Direct Message To Other Users With Your Bot
+DM_ID=$(curl -H "Authorization: Bot $bot_token" -H "Content-Type: application/json" \
+-X POST -d "{\"recipient_id\": \"$user_id\"}" \
+"https://discord.com/api/v9/users/@me/channels" | sed -n 's/.*"id":"\([^"]*\)".*"type":1.*/\1/p')
+
+
+curl -H "Authorization: Bot $bot_token" -H "Content-Type: application/json" \
+-X POST -d "{\"content\":\"$message\"}" "https://discord.com/api/v9/channels/$DM_ID/messages"
